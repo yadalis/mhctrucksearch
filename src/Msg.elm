eb354d1b061628aa04274106fc8c52f7b8ed2030
Suresh Yadali <suresh.yadali@gmail.com>
2018-12-05 09:59:54 -0600

    more cleanup

--- src/Msg.elm
+++ src/Msg.elm
@@ -10,14 +10,6 @@ import Json.Encode as Encode
 type Msg
     --= OnFetchTrucks (WebData (Array Truck))
     = OnFetchTrucks (Result Error (List Truck) )
-    -- | FilterCDLNoCheckBoxClicked Bool
-    -- | FilterCDLYesCheckBoxClicked Bool
-    -- | FilterYearCheckBoxClicked Int Bool -- index year and userAction - bool must be the last arg, since onclick events send user action to that last var automatically to the message
-    -- | FilterMakeCheckBoxClicked Int Bool-- might want to use MakeSearchFitler type instead of individual field types -- index make resultCount userAction - bool must be the last arg, since onclick events send user action to that last var automatically to the message
-    -- | FilterModelCheckBoxClicked Int Bool -- might want to use ModelSearchFitler type instead of individual field types -- index make resultCount userAction - bool must be the last arg, since onclick events send user action to that last var automatically to the message
-    -- | FilterSalesStatusCheckBoxClicked Int Bool -- might want to use ModelSearchFitler type instead of individual field types -- index make resultCount userAction - bool must be the last arg, since onclick events send user action to that last var automatically to the message
-    -- | FilterSleeperRoofCheckBoxClicked Int Bool -- might want to use ModelSearchFitler type instead of individual field types -- index make resultCount userAction - bool must be the last arg, since onclick events send user action to that last var automatically to the message
-    -- | FilterSleeperBunkCheckBoxClicked Int Bool -- might want to use ModelSearchFitler type instead of individual field types -- index make resultCount userAction - bool must be the last arg, since onclick events send user action to that last var automatically to the message
     | FilterCheckBoxClicked Int SearchFilterCustomType Bool -- might want to use ModelSearchFitler type instead of individual field types -- index make resultCount userAction - bool must be the last arg, since onclick events send user action to that last var automatically to the message
     | SearchString String
     | SearchPressed
