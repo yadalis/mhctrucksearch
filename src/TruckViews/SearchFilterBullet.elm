2c4cd94fb8c59546db37737c4321d43cf9de842f
Suresh Yadali <suresh.yadali@gmail.com>
2018-12-09 14:35:59 -0600

    fixed height issue search filter bullets X mark

--- src/TruckViews/SearchFilterBullet.elm
+++ src/TruckViews/SearchFilterBullet.elm
@@ -24,14 +24,14 @@ searchFilterBulletView filterList =
     filterList
         |> Array.map (\sf -> --el [bw 1, brc 185 185 185,  bc 195 195 195,pd 3] (
                             if sf.userAction then 
-                                row[ bw 0, pd 3, spx -5]
+                                row[ bw 0, pd 3 ]
                                 [
                                     column[][
                                         --el [bw 1, Font.size 14, brc 97 97 97, fc  250 250 250,  bc 105 105 125,pd 5, Font.light] (textValue <| sf.searchFilterKey)
-                                        el [bw 0, Font.size 16, brc 97 97 97, fc  190 5 30,  bc 231 231 231 ,pd 6, Font.light] (textValue <| sf.searchFilterKey)
+                                        el [bw 0, Font.size 15, brc 97 97 97, fc  190 5 30,  bc 231 231 231 ,pd 6, Font.light, hf] (textValue <| sf.searchFilterKey)
                                     ]
                                     ,column[][
-                                        Input.button ( [ hf, bw 0, brc 97 97 97,  bc 231 231 231 ,pd 6])
+                                        Input.button ( [ bw 0, brc 97 97 97,  bc 231 231 231 ,pd 6])
                                         { 
                                             onPress = Just (FilterCheckBoxClicked sf.index sf.filterCategory False )                                                       
                                             ,label = (el [wpx 15, hpx 15, Font.size 14,centerX, centerY, fc 300 300 300, bc 150 150 150] <| textValue <| "x")
